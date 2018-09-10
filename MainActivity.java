ac7b3ed2e00eebe27c593c7f710d1ee3f57eb7fa
shachi <shachi.shah@einfochips.com>
2018-09-10 16:57:51 +0530

    Initial Bluetooth Enabling and Pair Devices.

--- MainActivity.java
+++ MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
                 mMsg.setText(" ");
                 if(mBluetoothAdapter==null){
+
                     Toast.makeText(MainActivity.this, "No Bluetooth Device Found", Toast.LENGTH_SHORT).show();
                 }
                 else{
