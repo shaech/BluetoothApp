402564442db02a448e4e67e3e335af8b4771c249
shachi <shachi.shah@einfochips.com>
2018-09-10 17:02:43 +0530

    Initial Bluetooth Enabling and Pair Devices.

--- MainActivity.java
+++ MainActivity.java
@@ -30,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         mMsg = findViewById(R.id.textMessage);
 
 
-
         mstartBT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
